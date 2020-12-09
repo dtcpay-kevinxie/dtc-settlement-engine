09f565ba64b8eaee8e30dec58e91fd6adc02d931
Henry Young <henry.young@dtc.top>
2020-12-09 18:50:02 +0800

    UPDATE: @EnableCommonNotification

--- src/main/java/top/dtc/settlement/Application.java
+++ src/main/java/top/dtc/settlement/Application.java
@@ -3,10 +3,7 @@ package top.dtc.settlement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import top.dtc.common.core.DtcApplication;
-import top.dtc.common.core.config.EnableFastjsonHttpMessageConverter;
-import top.dtc.common.core.config.EnableMultipart;
-import top.dtc.common.core.config.EnableUnirestJSON;
-import top.dtc.common.core.config.EnableWeb;
+import top.dtc.common.core.config.*;
 import top.dtc.common.core.data.config.EnableDataAspect;
 import top.dtc.data.core.core.config.EnableDataCore;
 import top.dtc.data.finance.core.config.EnableDataFinance;
@@ -22,6 +19,7 @@ import top.dtc.data.risk.core.config.EnableDataRisk;
 @EnableUnirestJSON
 @EnableMultipart
 @EnableAsync
+@EnableCommonNotification
 public class Application {
 
     public static void main(String[] args) {
