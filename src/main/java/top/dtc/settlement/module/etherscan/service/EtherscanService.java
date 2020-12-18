2581ed101b135d34f2645e83f1850bf9e83f4405
LIN XIN <sam.lin@dtc.top>
2020-12-18 18:00:47 +0800

    Update : Standardize to lower case

--- src/main/java/top/dtc/settlement/module/etherscan/service/EtherscanService.java
+++ src/main/java/top/dtc/settlement/module/etherscan/service/EtherscanService.java
@@ -33,10 +33,10 @@ public class EtherscanService {
             if (respObject != null && respObject.result != null) {
                 String txnInput = respObject.result.input;
                 String methodId = txnInput.substring(0, 10);
-                String addressTo = txnInput.substring(10, 74);
+                String addressTo = txnInput.substring(10, 74).toLowerCase();
                 String value = txnInput.substring(74);
                 log.debug("methodId={} \n addressTo={} \n value={}", methodId, addressTo, value);
-                if (!addressTo.contains(bankAccount.substring(2))) { // remove "0x" from address
+                if (!addressTo.contains(bankAccount.substring(2).toLowerCase())) { // remove "0x" from address
                     throw new ValidationException("Transaction Recipient Unmatched");
                 }
                 if (new BigInteger(value, 16).compareTo(amount.multiply(new BigDecimal(1000000)).toBigInteger()) != 0) {
