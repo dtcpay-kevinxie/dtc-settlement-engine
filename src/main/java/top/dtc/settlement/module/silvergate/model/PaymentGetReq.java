60478d21608cea854df690c527daeed21bc9890f
kevin.xie <44841113+KevinSheeran@users.noreply.github.com>
2021-03-02 11:45:15 +0800

    UPDATE: update silvergate paymentGetReq

--- src/main/java/top/dtc/settlement/module/silvergate/model/PaymentGetReq.java
+++ src/main/java/top/dtc/settlement/module/silvergate/model/PaymentGetReq.java
@@ -1,10 +1,7 @@
 package top.dtc.settlement.module.silvergate.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.io.Serializable;
 
 /**
  * User: kevin.xie<br/>
@@ -12,21 +9,14 @@ import java.io.Serializable;
  * Time: 20:01<br/>
  */
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class PaymentGetReq implements Serializable {
+public class PaymentGetReq {
 
-    @JsonProperty("account_number")
     public String accountNumber;// required
-    @JsonProperty("payment_id")
     public String paymentId;
-    @JsonProperty("begin_date")
     public String beginDate;
-    @JsonProperty("end_date")
     public String endDate;
-    @JsonProperty("sort_order")
     public String sortOrder;
-    @JsonProperty("page_size")
     public Integer pageSize;
-    @JsonProperty("page_number")
     public Integer pageNumber;
+
 }
