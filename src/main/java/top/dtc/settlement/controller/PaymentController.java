3559d6d4ab6c78d225ba62213dffd3ee0cdccf0f
kevin.xie <44841113+KevinSheeran@users.noreply.github.com>
2021-03-02 14:38:46 +0800

    UPDATE: update paymentController

--- src/main/java/top/dtc/settlement/controller/PaymentController.java
+++ src/main/java/top/dtc/settlement/controller/PaymentController.java
@@ -11,8 +11,6 @@ import top.dtc.settlement.model.api.ApiResponse;
 import top.dtc.settlement.module.silvergate.model.*;
 import top.dtc.settlement.module.silvergate.service.SilvergateApiService;
 
-import java.io.IOException;
-
 /**
  * User: kevin.xie<br/>
  * Date: 22/02/2021<br/>
@@ -27,7 +25,7 @@ public class PaymentController {
     SilvergateApiService apiService;
 
     @GetMapping("/get-access-token")
-    public ApiResponse<?> getAccessToken() throws IOException, InterruptedException {
+    public ApiResponse<?> getAccessToken() {
         //Get accessToken and saved in Redis cache
         String accessToken = apiService.acquireAccessToken();
         log.info("getAccessToken: {}", accessToken);
@@ -37,6 +35,7 @@ public class PaymentController {
     @GetMapping("/account/get-account-balance")
     public ApiResponse<?> getAccountBalance(@RequestBody AccountBalanceReq accountBalanceReq) throws JsonProcessingException {
         AccountBalanceResp accountBalance = apiService.getAccountBalance(accountBalanceReq);
+        log.info("/account/balance request: {}", accountBalanceReq);
         return new ApiResponse<>(ApiHeaderConstant.SUCCESS, accountBalance);
     }
 
@@ -44,36 +43,41 @@ public class PaymentController {
     @GetMapping("/account/get-account-history")
     public ApiResponse<?> getAccountHistory(@RequestBody AccountHistoryReq accountHistoryReq) throws JsonProcessingException {
         AccountHistoryResp accountHistory = apiService.getAccountHistory(accountHistoryReq);
+        log.info("/account/history request: {}", accountHistoryReq);
         return new ApiResponse<>(ApiHeaderConstant.SUCCESS, accountHistory);
     }
 
     @GetMapping("/account/get-account-list")
     public ApiResponse<?> getAccountList(@RequestParam(name = "sequenceNumber", required = false) String sequenceNumber) throws JsonProcessingException {
         AccountListResp accountList = apiService.getAccountList(sequenceNumber);
+        log.info("/account/list request: {}", sequenceNumber);
         return new ApiResponse<>(ApiHeaderConstant.SUCCESS, accountList);
     }
 
     @PostMapping("/payment/post")
     public ApiResponse<?> postPayment(@RequestBody PaymentPostReq paymentPostReq) throws JsonProcessingException {
         PaymentPostResp paymentPostResp = apiService.initialPaymentPost(paymentPostReq);
+        log.info("[POST] payment request: {}", paymentPostReq);
         if (!ObjectUtils.isEmpty(paymentPostResp)) {
             return new ApiResponse<>(ApiHeaderConstant.SUCCESS, paymentPostResp);
         }
         return new ApiResponse<>(ApiHeaderConstant.COMMON.API_UNKNOWN_ERROR);
     }
 
-
     @PutMapping("/payment/put")
     public ApiResponse<?> putPayment(@RequestBody PaymentPutReq paymentPutReq) throws JsonProcessingException {
         PaymentPutResp paymentPutResp = apiService.initialPaymentPut(paymentPutReq);
+        log.info("[PUT] payment request: {}", paymentPutReq);
         if (!ObjectUtils.isEmpty(paymentPutResp)) {
             return new ApiResponse<>(ApiHeaderConstant.SUCCESS, paymentPutResp);
         }
         return new ApiResponse<>(ApiHeaderConstant.COMMON.API_UNKNOWN_ERROR);
     }
+
     @GetMapping("/payment/get")
     public ApiResponse<?> getPayment(@RequestBody PaymentGetReq paymentGetReq) throws JsonProcessingException {
         PaymentGetResp paymentDetails = apiService.getPaymentDetails(paymentGetReq);
+        log.info("[GET] /payment/get request: {}", paymentGetReq);
         if (!ObjectUtils.isEmpty(paymentDetails)) {
             return new ApiResponse<>(ApiHeaderConstant.SUCCESS, paymentDetails);
         }
@@ -83,23 +87,26 @@ public class PaymentController {
     @DeleteMapping("/webhooks/delete/{webHookId}")
     public ApiResponse<?> webHooksDelete(@PathVariable(value = "webHookId") String webHookId) {
         String result = apiService.webhooksDelete(webHookId);
+        log.info("[GET] webhooks/delete request: {}", webHookId);
         if (!StringUtils.isBlank(result)) {
             return new ApiResponse<>(ApiHeaderConstant.SUCCESS, result);
         }
         return new ApiResponse<>(ApiHeaderConstant.COMMON.API_UNKNOWN_ERROR);
     }
 
-    @GetMapping("/webhooks/get/")
+    @GetMapping("/webhooks/get")
     public ApiResponse<?> webHooksGet(@RequestBody WebHooksGetReq webHooksGetReq) throws JsonProcessingException {
         WebHooksGetRegisterResp[] webHooksGetRegisterResp = apiService.webHooksGet(webHooksGetReq);
+        log.info("webhooks/get request: {}", webHooksGetReq);
         if (!ObjectUtils.isEmpty(webHooksGetRegisterResp)) {
             return new ApiResponse<>(ApiHeaderConstant.SUCCESS, webHooksGetRegisterResp);
         }
         return new ApiResponse<>(ApiHeaderConstant.COMMON.API_UNKNOWN_ERROR);
     }
 
-    @PostMapping("/webhooks/register/")
+    @PostMapping("/webhooks/register")
     public ApiResponse<?> webHooksRegister(@RequestBody WebHooksRegisterReq webHooksRegisterReq) throws JsonProcessingException {
+        log.info("webhooks/register request: {}", webHooksRegisterReq);
         WebHooksGetRegisterResp webHooksRegisterResp = apiService.webHooksRegister(webHooksRegisterReq);
         return new ApiResponse<>(ApiHeaderConstant.SUCCESS, webHooksRegisterResp);
     }
