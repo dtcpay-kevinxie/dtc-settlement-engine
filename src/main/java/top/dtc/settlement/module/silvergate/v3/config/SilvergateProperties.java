206e82b6a1fb707d9ab18b5f86126b1b6fd02e7c
Kevin Xie <kevin.xie@dtc.top>
2022-12-08 17:32:43 +0800

    UPDATE: Update silvergate v3 configuration

--- src/main/java/top/dtc/settlement/module/silvergate/v3/config/SilvergateProperties.java
+++ src/main/java/top/dtc/settlement/module/silvergate/v3/config/SilvergateProperties.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Setter
 @Component
-@ConfigurationProperties("silvergate")
+@ConfigurationProperties("silvergate.v3")
 public class SilvergateProperties {
 
     public String subscriptionKey;
